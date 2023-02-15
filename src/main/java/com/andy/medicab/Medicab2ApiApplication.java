c9516e3f39b042b379f05be7eb5bfd4aee160cbb
andy <josueandy5@gmail.com>
2023-02-15 14:17:33 +0100

    up  2

--- src/main/java/com/andy/medicab/Medicab2ApiApplication.java
+++ src/main/java/com/andy/medicab/Medicab2ApiApplication.java
@@ -1,13 +1,26 @@
-package com.andy.medicab.medicab2api;
+package com.andy.medicab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import static com.andy.medicab.contoller.Utils.BASE_URL;
+
+@CrossOrigin({BASE_URL})
+@RestController
 @SpringBootApplication
 public class Medicab2ApiApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Medicab2ApiApplication.class, args);
+    }
+    @RequestMapping("/")
+    public String home() {
+
+        return "API MEDICAB v2.0.0";
     }
 
 }
